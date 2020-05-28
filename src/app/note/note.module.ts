90a2052b9a35c09441e2e572dae21a5b55403701
yu728 <yuu.watanabe0728@gmail.com>
2020-05-28 14:42:19 +0900

    feat(note): form作成 #9

--- src/app/note/note.module.ts
+++ src/app/note/note.module.ts
@@ -3,9 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { NoteRoutingModule } from './note-routing.module';
 import { NoteComponent } from './note/note.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [NoteComponent],
-  imports: [CommonModule, NoteRoutingModule],
+  imports: [
+    CommonModule,
+    NoteRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
 })
 export class NoteModule {}
--- src/app/note/note/note.component.ts
+++ src/app/note/note/note.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
-  styleUrls: ['./note.component.scss']
+  styleUrls: ['./note.component.scss'],
 })
 export class NoteComponent implements OnInit {
+  form = this.fb.group({
+    todo: ['', [Validators.required, Validators.maxLength(1000)]],
+    done: ['', [Validators.required, Validators.maxLength(1000)]],
+    log: ['', [Validators.required, Validators.maxLength(1000)]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
