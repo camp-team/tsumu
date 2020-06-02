cea5ee6027bfffcf56e9ac14513a1521065b00b1
yu728 <yuu.watanabe0728@gmail.com>
2020-06-02 18:55:33 +0900

    feat(timeline): noteデータ表示

--- src/app/timeline/timeline/timeline.component.ts
+++ src/app/timeline/timeline/timeline.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/services/note.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-timeline',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline.component.scss']
 })
 export class TimelineComponent implements OnInit {
+  notes$ = this.noteService.getAllNotes().pipe(
+    tap(data => console.log(data))
+  );
 
-  constructor() { }
+  constructor(private noteService: NoteService) { }
 
   ngOnInit(): void {
   }
