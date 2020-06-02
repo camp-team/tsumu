fc8e9ac737d3b01b4e29e9dd16c921a08af22e57
yu728 <yuu.watanabe0728@gmail.com>
2020-06-02 13:18:32 +0900

    feat(timeline):timeline-cardコンポーネントの追加

---
+++ src/app/timeline/timeline-card/timeline-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/interfaces/user';
+import { Note } from 'src/app/interfaces/note';
+
+@Component({
+  selector: 'app-timeline-card',
+  templateUrl: './timeline-card.component.html',
+  styleUrls: ['./timeline-card.component.scss']
+})
+export class TimelineCardComponent implements OnInit {
+  @Input() note: Note;
+
+  user$: Observable<User> = this.authService.user$;
+
+  constructor(private authService: AuthService) {
+    // console.log(this.note, 'noteの中身');
+  }
+
+  ngOnInit(): void {
+  }
+
+}
