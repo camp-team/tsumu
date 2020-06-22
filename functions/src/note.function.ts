54ce0e7fb61eb1eea4cc45c0c2e0854d372b8c68
yu728 <yuu.watanabe0728@gmail.com>
2020-06-22 14:20:41 +0900

    feat(functions): Algoliaにnotesのデータを保存

---
+++ functions/src/note.function.ts
@@ -0,0 +1,28 @@
+import * as functions from 'firebase-functions';
+import { Algolia } from './utils/algolia';
+
+const algolia = new Algolia();
+
+export const createNote = functions
+  .region('asia-northeast1')
+  .firestore.document('notes/{id}')
+  .onCreate(snap => {
+    const data = snap.data();
+    return algolia.saveRecord({
+      indexName: 'notes',
+      largeConcentKey: 'body',
+      data
+    })
+  })
+
+export const deleteNote = functions
+  .region('asia-northeast1')
+  .firestore.document('notes/{id}')
+  .onDelete(snap => {
+    const data = snap.data();
+    if (data) {
+      algolia.removeRecord('notes', data.id);
+    } else {
+      return;
+    }
+  });
