51469eed9fc1c159c363310e7e01e3341e00b087
yu728 <yuu.watanabe0728@gmail.com>
2020-07-29 09:36:22 +0900

    feat: エミュレータ設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFireFunctionsModule, REGION } from '@angular/fire/functions';
+import { AngularFireFunctionsModule, REGION, ORIGIN } from '@angular/fire/functions';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { RouterModule } from '@angular/router';
@@ -58,7 +58,13 @@ import { MatDividerModule } from '@angular/material/divider';
     MatDividerModule,
     RouterModule,
   ],
-  providers: [{ provide: REGION, useValue: 'asia-northeast1' }],
+  providers: [
+    { provide: REGION, useValue: 'asia-northeast1' },
+    {
+      provide: ORIGIN,
+      useValue: environment.production ? undefined : 'http://localhost:5001',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
