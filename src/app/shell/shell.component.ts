85c5f550939c016b386d69716903eac003dd01bc
yu728 <yuu.watanabe0728@gmail.com>
2020-08-11 11:13:52 +0900

    feature(header): sidenavのmode追加

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { DrawerService } from '../services/drawer.service';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
@@ -10,16 +10,34 @@ import { User } from '../interfaces/user';
   styleUrls: ['./shell.component.scss']
 })
 export class ShellComponent implements OnInit {
+  sidenavMode: string;
   isOpened: boolean;
   title = 'tsumu';
   user$: Observable<User> = this.authService.user$;
 
-  constructor(private drawerService: DrawerService, private authService: AuthService) {
+  constructor(
+    private drawerService: DrawerService,
+    private authService: AuthService,
+    private ngZone: NgZone) {
     this.drawerService.toggle();
     this.drawerService.isOpen$.subscribe(opened => this.isOpened = opened);
+    window.onresize = (e) => {
+      this.ngZone.run(() => {
+        this.handleResizeWindow(window.innerWidth);
+      });
+    };
   }
 
   ngOnInit(): void {
+    this.handleResizeWindow(window.innerWidth);
+  }
+
+  handleResizeWindow(width: number) {
+    if (800 < width) {
+      this.sidenavMode = 'side';
+    } else {
+      this.sidenavMode = 'over';
+    }
   }
 
 }
